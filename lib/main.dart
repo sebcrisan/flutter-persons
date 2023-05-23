cde61822c12f3b20c0993bc35177b2a6a035d3fb
sebcrisan <sebastiaancrisan@gmail.com>
2023-05-23 13:24:57 +0200

    feat: build UI

--- lib/main.dart
+++ lib/main.dart
@@ -191,6 +191,41 @@ class MyHomePage extends ConsumerWidget {
       appBar: AppBar(
         title: const Text('Persons'),
       ),
+      body: Consumer(
+        builder: (context, ref, child) {
+          final dataModel = ref.watch(peopleProvider);
+          return ListView.builder(
+            itemCount: dataModel.count,
+            itemBuilder: (context, index) {
+              final person = dataModel.people[index];
+              return ListTile(
+                title: GestureDetector(
+                  onTap: () async {
+                    final updatedPerson =
+                        await createOrUpdatePersonDialog(context, person);
+                    if (updatedPerson != null) {
+                      dataModel.update(updatedPerson);
+                    }
+                  },
+                  child: Text(person.displayName),
+                ),
+              );
+            },
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          final person = await createOrUpdatePersonDialog(
+            context,
+          );
+          if (person != null) {
+            final dataModel = ref.read(peopleProvider);
+            dataModel.add(person);
+          }
+        },
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
